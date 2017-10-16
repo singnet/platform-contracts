de726f4b33ed56180fe4cf1cfc613c8f02e38249
Marco Argentieri <marco@vulpem.com>
2017-10-16 18:14:25 +0200

    Added kovan network in truffle.js

--- truffle.js
+++ truffle.js
@@ -5,5 +5,10 @@ module.exports = {
       port: 8545,
       network_id: "*" // Match any network id
     }
+  },
+  kovan: {
+    host: "localhost",
+    port: 8549,
+    network_id: "*" // Match any network id
   }
 };
