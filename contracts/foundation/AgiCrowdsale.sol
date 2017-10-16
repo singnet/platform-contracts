6df2abb993d4ea157ef80d4b1cec2f26c93c909c
Marco Argentieri <marco@vulpem.com>
2017-10-17 00:17:51 +0200

    Polished

--- contracts/foundation/AgiCrowdsale.sol
+++ contracts/foundation/AgiCrowdsale.sol
@@ -7,69 +7,34 @@ import 'zeppelin-solidity/contracts/token/StandardToken.sol';
 
 /**
  * @title AgiCrowdsale
- * @dev Modified from OpenZeppelin's Crowdsale.sol, RefundableCrowdsale.sol,
- * CappedCrowdsale.sol, and FinalizableCrowdsale.sol
- * Uses PausableToken rather than MintableToken.
- *
- * Requires that tokens for sale (entire supply minus team's portion) be deposited.
+ * @dev Built on top OpenZeppelin
  */
 contract AgiCrowdsale is Ownable {
   using SafeMath for uint256;
 
-  // Token allocations
   mapping (address => uint256) public allocations;
-
-  // Whitelisted investors
   mapping (address => bool) public whitelist;
 
-  // manual early close flag
-  bool public isFinalized = false;
-
-  // cap for crowdsale in wei
   uint256 public cap;
-
-  // minimum amount of funds to be raised in weis
   uint256 public goal;
-
-  // refund vault used to hold funds while crowdsale is running
+  uint256 public rate;
+  
+  StandardToken public token;
+  
   RefundVault public vault;
 
-  // The token being sold
-  StandardToken public token;
-
-  // start and end timestamps where contributions are allowed (both inclusive)
   uint256 public startTime;
   uint256 public endTime;
 
-  // address where funds are collected
   address public wallet;
-
-  // address to hold team / advisor tokens until vesting complete
-  address public safe;
-
-  // how many token units a buyer gets per wei
-  uint256 public rate;
-
-  // amount of raised money in wei
+  
+  bool public isFinalized = false;
   uint256 public weiRaised;
 
-  /**
-   * event for token purchase logging
-   * @param purchaser who paid for the tokens
-   * @param beneficiary who got the tokens
-   * @param value weis paid for purchase
-   * @param amount amount of tokens purchased
-   */
-  event TokenPurchase(address indexed purchaser, address indexed beneficiary, uint256 value, uint256 amount);
 
-  /**
-   * event for token redemption logging
-   * @param beneficiary who got the tokens
-   * @param amount amount of tokens redeemed
-   */
-  event TokenRedeem(address indexed beneficiary, uint256 amount);
+  event TokenBuy(address indexed buyer, address indexed beneficiary, uint256 value, uint256 amount);
+  event TokenRelease(address indexed beneficiary, uint256 amount);
 
-  // termination early or otherwise
   event Finalized();
 
   function AgiCrowdsale(address _token, uint256 _startTime, uint256 _endTime, uint256 _rate, uint256 _cap, uint256 _goal, address _wallet) {
@@ -81,47 +46,22 @@ contract AgiCrowdsale is Ownable {
     require(_goal > 0);
 
     vault = new RefundVault(_wallet);
-    goal = _goal;
+    wallet = _wallet;
     token = StandardToken(_token);
     startTime = _startTime;
     endTime = _endTime;
     rate = _rate;
     cap = _cap;
     goal = _goal;
-    wallet = _wallet;
   }
 
-  // fallback function can be used to buy tokens
+  // fallback function 
   function() payable {
-    buyTokens(msg.sender);
+    purchaseTokens(msg.sender);
   }
 
-  // Day 1: 1 ETH = 1,200 POLY
-  // Day 2: 1 ETH = 1,100 POLY
-  // Day 3: 1 ETH = 1,000 POLY
-  function calculateBonus(uint256 weiAmount) internal returns (uint256) {
-    uint256 DAY1 = startTime + 24 hours;
-    uint256 DAY2 = DAY1 + 24 hours;
-    uint256 DAY3 = DAY2 + 24 hours;
-    uint256 bonusTokens;
-    uint256 bonusRate;
-
-    if (getBlockTimestamp() > startTime && getBlockTimestamp() < DAY1) {
-      bonusRate =  1200;
-      // bonusRate =  0.0000000000000012;
-    } else if (getBlockTimestamp() > DAY1 && getBlockTimestamp() < DAY2) {
-      bonusRate =  1100;
-    } else if (getBlockTimestamp() > DAY2 && getBlockTimestamp() < DAY3) {
-      bonusRate =  1000;
-    }
-    bonusTokens = weiAmount.mul(bonusRate);
-    return bonusTokens;
-  }
-
-  /// @notice interface for founders to whitelist investors
-  /// @param _addresses array of investors
-  /// @param _status enable or disable
-  function whitelistAddresses(address[] _addresses, bool _status) public onlyOwner {
+  // whitelist contirbutors
+  function whitelist(address[] _addresses, bool _status) public onlyOwner {
     for (uint256 i = 0; i < _addresses.length; i++) {
         address investorAddress = _addresses[i];
         if (whitelist[investorAddress] == _status) {
@@ -133,23 +73,22 @@ contract AgiCrowdsale is Ownable {
 
   // low level token purchase function
   // caution: tokens must be redeemed by beneficiary address
-  function buyTokens(address beneficiary) payable {
+  function purchaseTokens(address beneficiary) payable {
     require(whitelist[beneficiary]);
     require(beneficiary != 0x0);
     require(validPurchase());
     // calculate token amount to be purchased
     uint256 weiAmount = msg.value;
     uint256 tokens = weiAmount.mul(rate);
-    uint256 bonusTokens = calculateBonus(weiAmount);
-    tokens = tokens.add(bonusTokens);
+
 
     // update state
     weiRaised = weiRaised.add(weiAmount);
 
-    // allocate tokens to purchaser
+    // allocate tokens to buyer
     allocations[beneficiary] = tokens;
 
-    TokenPurchase(msg.sender, beneficiary, weiAmount, tokens);
+    TokenBuy(msg.sender, beneficiary, weiAmount, tokens);
 
     forwardFunds();
   }
@@ -163,12 +102,12 @@ contract AgiCrowdsale is Ownable {
     uint256 amount = token.balanceOf(this);
     require(amount > 0);
 
-    // send tokens to purchaser
+    // send tokens to buyer
     uint256 tokens = allocations[msg.sender];
     allocations[msg.sender] = 0;
     require(token.transfer(msg.sender, tokens));
 
-    TokenRedeem(msg.sender, tokens);
+    TokenRelease(msg.sender, tokens);
   }
 
   // redeem tokens (admin fallback)
@@ -180,16 +119,15 @@ contract AgiCrowdsale is Ownable {
     uint256 amount = token.balanceOf(this);
     require(amount > 0);
 
-    // send tokens to purchaser
+    // send tokens to buyer
     uint256 tokens = allocations[beneficiary];
     allocations[beneficiary] = 0;
     require(token.transfer(beneficiary, tokens));
 
-    TokenRedeem(beneficiary, tokens);
+    TokenRelease(beneficiary, tokens);
   }
 
-  // send ether to the fund collection wallet
-  // override to create custom fund forwarding mechanisms
+  // send ether to vault until the sale ends
   function forwardFunds() internal {
     vault.deposit.value(msg.value)(msg.sender);
   }
--- contracts/tokens/SingularityNetToken.sol
+++ contracts/tokens/SingularityNetToken.sol
@@ -4,27 +4,25 @@ import "zeppelin-solidity/contracts/token/StandardToken.sol";
 import "zeppelin-solidity/contracts/lifecycle/Pausable.sol";
 
 /**
- * @title Singularity Network Token
- * @dev ERC20 Singularity Network Token (SIN)
+ * @title SingularityNET Token
+ * @dev ERC20 SingularityNET Token (AGI)
  *
- * SIN Tokens are divisible by 1e8 (100,000,000) base
- * units referred to as 'unit'.
+ * AGI Tokens are divisible by 1e8 (100,000,000) base
+ * spikes referred to as 'spike'.
  *
- * SIN are displayed using 8 decimal places of precision.
+ * AGI are displayed using 8 decimal places of precision.
  *
- * 1 SIN is equivalent to:
- *   100000000 == 1 * 10**8 == 1e8 == One Hundred Million units
+ * 1 AGI is equivalent to:
+ *   100000000 == 1 * 10**8 == 1e8 == One Hundred Million spikes
  *
- * 1 Billion SIN (total supply) is equivalent to:
- *   100000000000000000 == 1000000000 * 10**8 == 1e17 == One Hundred Quadrillion units
+ * 1 Billion AGI (total supply) is equivalent to:
+ *   100000000000000000 == 1000000000 * 10**8 == 1e17 == One Hundred Quadrillion spikes
  *
- * All initial SIN units are assigned to the creator of
- * this contract.
  *
  */
 contract SingularityNetToken is StandardToken, Pausable {
 
-    string public constant NAME = "Singularity Network Token";
+    string public constant NAME = "SingularityNET Token";
     string public constant SYMBOL = "AGI";
     uint8 public constant DECIMALS = 8;
     uint256 public constant INITIAL_SUPPLY = 1000000000 * 10**uint256(DECIMALS);
