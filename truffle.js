c535d5977a515e07b74acf695ef40cb269e8243d
Marco Argentieri <marco@vulpem.com>
2017-11-01 17:47:47 +0100

    truffle.js rolled back, hots wants an ip address or localhost

--- truffle.js
+++ truffle.js
@@ -1,13 +1,13 @@
 module.exports = {
     networks: {
         development: {
-            host: "testrpc",
+            host: "localhost",
             port: 8545,
             network_id: "*" // Match any network id
         }
     },
     kovan: {
-        host: "testrpc",
+        host: "localhost",
         port: 8549,
         network_id: "*" // Match any network id
     }
