0595d73678fdee14fff7e38e0a5aa45eed1897e0
Curtis Faith <curtis.m.faith@gmail.com>
2017-12-08 18:33:48 -0500

    Add Agent and MarketJob deployment

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,23 +1,29 @@
 const fs = require('fs')
 
+const Agent = artifacts.require('agent/Agent.sol')
 const AgentFactory = artifacts.require('agent/AgentFactory.sol')
 const AgentRegistry = artifacts.require('registries/AgentRegistry.sol')
+const MarketJob = artifacts.require('market/MarketJob.sol')
 const MarketJobFactory = artifacts.require('market/MarketJobFactory.sol')
 const SingularityNetToken = artifacts.require('tokens/SingularityNetToken.sol')
 const TokenVestingFactory = artifacts.require('tokens/TokenVestingFactory.sol')
 
 module.exports = function(deployer, network, accounts) {
   deployer.deploy([
+    Agent,
     AgentFactory,
     AgentRegistry,
+    MarketJob,
     MarketJobFactory,
     SingularityNetToken,
     TokenVestingFactory
   ]).then(() => {
     const fileName = "addresses.json"
     const content = {
+      Agent: Agent.address,
       AgentFactory: AgentFactory.address,
       AgentRegistry: AgentRegistry.address,
+      MarketJob: MarketJob.address,
       MarketJobFactory: MarketJobFactory.address,
       SingularityNetToken: SingularityNetToken.address,
       TokenVestingFactory: TokenVestingFactory.address
