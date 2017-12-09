ff788026bf13dab98b3a6c7847e8bef9334d1002
Curtis Faith <curtis.m.faith@gmail.com>
2017-12-09 07:55:56 -0500

    Added support for docker contacting host ganache

--- truffle.js
+++ truffle.js
@@ -4,15 +4,20 @@ const host = "http://localhost:8545"//NODE
 
 module.exports = {
   networks: {
+    docker_host: {
+      host: "192.168.0.1",
+      port: 7545,
+      network_id: "*"
+    },
     ganache: {
       host: "localhost",
       port: 7545,
-      network_id: "*" // Match any network id
+      network_id: "*"
     },
     testrpc: {
       host: "testrpc",
       port: 8545,
-      network_id: "*" // Match any network id
+      network_id: "*"
     },
     kovan: {
       provider: () => {
