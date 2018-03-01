1dbbfee8b1a9708655cc1b7deafe7de33d3a19b0
Marco Argentieri <marco@vulpem.com>
2018-03-01 14:55:03 +0100

    Refactor MarketJob to more lightweight contract with acceptance and rejection mechanism

--- contracts/market/Job.sol
+++ contracts/market/Job.sol
@@ -1,39 +1,66 @@
 pragma solidity ^0.4.18;
 
-contract Job {
-    uint256 public status = 0;
-    bytes32 public jobDescriptor;
-    bytes32 public jobResult;
+import './JobValidator.sol';
+/**
+ * @title The wrapped Job contract 
+ */
+contract Job is JobValidator {
 
-    uint256 public start;
-    uint256 public end;
+     /**
+    * @dev Kovan AGI token.
+    * https://kovan.etherscan.io/token/0x3b226ff6aad7851d3263e53cb7688d13a07f6e81#readContract
+    * ERC20Basic public constant AGI = ERC20Basic(0x3b226ff6aad7851d3263e53cb7688d13a07f6e81);
+    */  
 
-    address public payer;
-    address public payee;
+    /**
+     * @dev Cost in AGI for the exection of the job
+     * (10**8 cogs === 1 AGI)
+     */
+    uint256 public cost;
 
-    event JobStarted(address indexed payer, address indexed payee, bytes32 jobDescriptor, address jobAddress);
-    event JobCompleted(bytes32 jobResult, address jobAddress);
+    /**
+     * @dev Started event
+     */
+    event Started(address indexed payer, address indexed payee, bytes32 descriptor, address job);
 
-    function setJobStarted(address _payer, address _payee, bytes32 _jobDescriptor) public {
-        require(status < 1);
+     /**
+      * @dev Job constructor.
+      * @param _payer An agent who pays the job.
+      * @param _payee An agent to whom the job is paid.
+      * @param _cost Cost in AGI
+      */
+    function Job(address _payer, address _payee, bytes32 _descriptor, uint256 _cost, uint256 _reward) public {
+
         payer = _payer;
         payee = _payee;
-        jobDescriptor = _jobDescriptor;
-        status = 1;
+        descriptor = _descriptor;
+        cost = _cost;
+        reward = _reward;
+
         start = now;
 
-        JobStarted(payer, payee, jobDescriptor, address(this));
+        Started(payer, payee, descriptor, address(this));
     }
 
-    function setJobCompleted(bytes32 _jobResult) public {
+    /**
+     * @dev Set result of this Job
+     * @param _result Result data hash
+     */
+    function setResult(bytes32 _result) public returns (bool) {
         require(msg.sender == payee);
-        require(status < 2);        
-        status = 2;
-        jobResult = _jobResult;
+        require(descriptor.length > 0);
+        require(result.length == 0);
+
         end = now;
+ 
+        Result(_result);
+        result = _result;
 
-        JobCompleted(jobResult, address(this));
+        Completed();
 
+        return true;
     }
+
+
 
 }
---
+++ contracts/market/JobValidator.sol
@@ -0,0 +1,63 @@
+pragma solidity ^0.4.18;
+
+import './JobInterface.sol';
+
+/**
+ * @title The Job Validator contract 
+ */
+
+contract JobValidator is JobInterface {
+
+    /**
+     * @dev Reward for validator in AGI.
+     */
+    address public validator; 
+
+    /**
+     * @dev Validation status
+     * 0 == pending && 1 == accepted && 2 == rejected 
+     */
+    address public status = 0; 
+    
+    /**
+     * @dev Reward for validator in AGI.
+     */
+    uint256 public reward; 
+
+    /**
+     * @dev Completed event to notice the validators pool
+     */
+    event Completed();
+
+    /**
+     * @dev Approved event to notice the parties
+     */
+    event Accepted();
+
+    /**
+     * @dev Rejected event to notice the parties
+     */
+    event Rejected();
+
+
+    /**
+     * @dev Confirm job result
+     */
+    function accept() public {
+        validator = msg.sender;
+        status = 1;
+
+        Accepted();
+    }
+
+    /**
+     * @dev Reject job result
+     */
+    function reject() public {
+        validator = msg.sender;
+        status = 2;
+
+        Rejected();
+    }
+
+}
